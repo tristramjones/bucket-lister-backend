91ee57b214dbeac42744923e0f91e38174c787f9
trisjones <trisjones04@gmail.com>
2018-10-10 17:26:42 -0400

    Added the attractions table so models should now all be set up.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :location
+  has_many :attractions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_211624) do
+ActiveRecord::Schema.define(version: 2018_10_10_212400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "trip_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trip_id"], name: "index_attractions_on_trip_id"
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -38,4 +47,5 @@ ActiveRecord::Schema.define(version: 2018_10_10_211624) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "attractions", "trips"
 end
