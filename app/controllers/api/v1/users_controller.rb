7adc15f4940e661caa1cb1081165e8b804ffe275
trisjones <trisjones04@gmail.com>
2018-10-23 11:20:25 -0400

    Successfully sent post request to create a user from console. JWT token came back in response properly as well so it appears to be working.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
   def index
     @users = User.all.map{|user| {
       id: user.id,
-      name: user.name,
+      username: user.username,
       trips: user.trips,
       attractions: user.trips.map{|trip| trip.attractions},
       locations: user.locations
@@ -23,10 +24,15 @@ class Api::V1::UsersController < ApplicationController
     }
   end
 
+  def profile
+    render json: { user: current_user }, status: :accepted
+  end
+
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: @user }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+      get '/users', to: 'users#index'
       resources :locations
       resources :trips
       resources :attractions
