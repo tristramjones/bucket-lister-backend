3b79584eb6e77b222f8c666def627feb6a438c87
trisjones <trisjones04@gmail.com>
2018-10-23 10:15:57 -0400

    Migrations have been created and migrated so schema looks good.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :trips
   has_many :locations, through: :trips
-
-  validates :name, presence: true, uniqueness: true
+  
+  has_secure_password
+  validates :username, uniqueness: { case_sensitive: false }
+  # validates :name, presence: true, uniqueness: true
 end
